4e680d846304a521117d4ce2822d9ace5fde539c
chlin907 <chiahui.lin0816@gmail.com>
2018-06-20 20:53:41 -0700

    scaler is added. linear regression type correct

--- main.py
+++ main.py
@@ -10,17 +10,19 @@ import sklearn_helper
 def main():
     #================================ Define hyperparameters and models 
     # Dataset and problem statement
-    dataset_num = 2     # Int:        1 for 'breast_cancer'; 2 for 'iris'; 3 for 'boston'
-                        # Remark:     'brest_caner' is binary classfication for ['malignant' 'benign'] tumor
-                        #             'iris' is multiclass classfication for ['setosa' 'versicolor' 'virginica'] iris types
-                        #             'boston' is regression example to predict Boston housing price
-    mode = 'cls'        # Options:    'cls' for classification and 'reg' for regression
-
+    dataset_num = 3        # Int:     1 for 'breast_cancer'; 2 for 'iris'; 3 for 'boston'
+                           # Remark:  'brest_caner' is binary classfication for ['malignant' 'benign'] tumor
+                           #          'iris' is multiclass classfication for ['setosa' 'versicolor' 'virginica'] iris types
+                           #          'boston' is regression example to predict Boston housing price
+    mode = 'reg'           # Options: 'cls' for classification and 'reg' for regression
+    
+    run_grid_search = 'n'  # Str:     'y' or 'n'. To run grid search some default hyper parametes
+    scaler_std = 'n'      # Str:     'y' or 'n'. To enable scaling by mean and std
     # knn parameters
     num_neighbors = 2   # Definition.  Float. number of neighbors for knn
 
     # Linear model parameters
-    reg_mode = 'l1'     # Options:    Str. 'simple, 'l1', 'l2'. 
+    reg_mode = 'simple'     # Options:    Str. 'simple, 'l1', 'l2'. 
                         # Remark:     'l1' and 'l2' mean Lasso and Ridge regressoins.
     reg_param = 1       # Definition: Float. Regularization parameter
                         # Remark:     Represent alpha value in linear regression and is proportional to regularizaton strength
@@ -34,7 +36,7 @@ def main():
     num_estimator = 100    # Definition: Int. Num of tress to construct a forest
 
     # SVM parameters
-    pen_param = 1       # Definition: Float. Penalty parameter
+    pen_param = 0.1       # Definition: Float. Penalty parameter
 
     #================================ Code starts
     #================================ Data preparation: Convert features into a dataframe and label(y) into a series
@@ -46,16 +48,19 @@ def main():
     sklearn_helper.sklearn_dataset_info_print(sklearn_dataset)
 
     x_pd_dataframe = pd.DataFrame(sklearn_dataset.data, columns = sklearn_dataset.feature_names)
+    if scaler_std == 'y':
+        x_pd_dataframe = (x_pd_dataframe - x_pd_dataframe.mean()) / (x_pd_dataframe.std())
+
     y_pd_series = pd.Series(sklearn_dataset.target)
 
     #================================ Modeling with various models
     modeling = sklearn_helper.sklearn_model(x_pd_dataframe, y_pd_series)
 
-    modeling.sklearn_knn(n_neighbors = num_neighbors, mode = mode)
-    modeling.sklearn_linear_model(mode = mode, regular_mode = reg_mode, regular_param = reg_param)
-    modeling.sklearn_decision_tree(mode = mode, max_depth = num_max_depth)
-    modeling.sklearn_random_forest(mode = mode, max_depth = num_max_depth, n_estimators = num_estimator)
-    modeling.sklearn_svm(mode = mode, pen_param =pen_param)
+    modeling.sklearn_knn(n_neighbors = num_neighbors, mode = mode, grid_search = run_grid_search)
+    modeling.sklearn_linear_model(mode = mode, regular_mode = reg_mode, regular_param = reg_param, grid_search = run_grid_search)
+    modeling.sklearn_decision_tree(mode = mode, max_depth = num_max_depth, grid_search = run_grid_search)
+    modeling.sklearn_random_forest(mode = mode, max_depth = num_max_depth, n_estimators = num_estimator, grid_search = run_grid_search)
+    modeling.sklearn_svm(mode = mode, pen_param = pen_param, grid_search = run_grid_search)
 
 if __name__ == '__main__':
     main()
--- sklearn_helper.py
+++ sklearn_helper.py
@@ -25,6 +25,21 @@ def sklearn_dataset_info_print(sklearn_dataset):
 
 ####################################################################################
 class sklearn_model:
+    # Use for grid search only
+    _grid_param_knn = {'n_neighbors': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]}
+    _grid_param_svm = {'C': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 'gamma': [0.01, 0.05, 0.1, 0.5]}
+    _grid_param_dt = {
+         'min_samples_leaf': range(10, 60,10),
+         'max_depth': range(3, 14, 2),
+         'max_features': [2, 3],
+         'min_samples_split': range(50,201,20),
+    }
+    _grid_param_rf = {
+         'n_estimators' : [100, 200, 300, 400],
+         'max_depth': range(4, 10, 2),
+    }
+    _grid_param_logistic_reg = {'C':[0.01,0.05,0.1,0.5,1,5,10,50,100]}
+    _grid_param_ridge_lasso = {'alpha':[0.01,0.05,0.1,0.5,1,5,10,50,100]}
 
     def __init__(self, x_pd_dataframe_in, y_pd_series_in):
         self.x_pd_dataframe = x_pd_dataframe_in
@@ -33,9 +48,8 @@ class sklearn_model:
 
     def _train_test_split(self):
         from sklearn.model_selection import train_test_split
-
-        num_random_state = 0
-        x_train, x_test, y_train, y_test = train_test_split(self.x_pd_dataframe, self.y_pd_series, random_state=num_random_state)
+        #x_train, x_test, y_train, y_test = train_test_split(self.x_pd_dataframe, self.y_pd_series, test_size=0.2,  random_state=num_random_state)
+        x_train, x_test, y_train, y_test = train_test_split(self.x_pd_dataframe, self.y_pd_series)
 
         return x_train, x_test, y_train, y_test
 
@@ -44,14 +58,33 @@ class sklearn_model:
         return cross_val_score(ml_model, self.x_pd_dataframe, self.y_pd_series).mean()
 
     def _reporting(self, ml_method, ml_model):
+        k = 3
         print("========== ML training result")
         print('ML method: {}'.format(ml_method))
         print('Train score: {}'.format(ml_model.score(self.x_train, self.y_train)))
         print('Test score : {}'.format(ml_model.score(self.x_test, self.y_test)))
-        k = 10
-        print('{}-fold cv : {}'.format(k, self._cross_val(10, ml_model)))
+        print('{}-fold cv : {}'.format(k, self._cross_val(k, ml_model)))
 
-    def sklearn_knn(self, n_neighbors = 3, mode = 'cls'):
+    def _grid_search(self, sklearn_model, grid_param, mode):  # mode = 'reg' or 'cls
+        from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
+        cv_num = 10  # 10-fold cv
+        jobs = -1    # use all cpu's
+        if mode == 'reg':
+            param_search = GridSearchCV(estimator=sklearn_model, param_grid=grid_param, scoring='r2', cv=cv_num, n_jobs=jobs)
+        elif mode == 'cls':
+            param_search = GridSearchCV(estimator=sklearn_model, param_grid=grid_param, scoring='accuracy', cv=cv_num, n_jobs=jobs)
+        else:
+            raise Exception('Invalid mode in {}: '.format(sys._getframe().f_code.co_name), mode)
+
+        param_search.fit(self.x_pd_dataframe, self.y_pd_series)
+
+        print('Best params: %s' % param_search.best_params_)
+        print('Best training accuracy: %.3f' % param_search.best_score_)
+
+        return param_search.best_estimator_
+
+
+    def sklearn_knn(self, n_neighbors = 3, mode = 'cls', grid_search='y'):
         from sklearn.neighbors import KNeighborsClassifier, KNeighborsRegressor
     
         if mode == 'cls':
@@ -65,13 +98,18 @@ class sklearn_model:
 
         ml_method_ext = ' ({})'.format(mode) if mode is not None else ''
         self._reporting(sys._getframe().f_code.co_name + ml_method_ext, ml_model)
-    
-    def sklearn_linear_model(self, mode = 'reg', regular_mode = 'simple', regular_param = 1.0):
+
+        if grid_search == 'y':
+            grid_param = self._grid_param_knn
+            print('Grid search conditions: {}'.format(grid_param))
+            self._grid_search(sklearn_model=ml_model, grid_param=grid_param, mode = mode)
+
+    def sklearn_linear_model(self, mode = 'reg', regular_mode = 'simple', regular_param = 1.0, grid_search='y'):
         from sklearn.linear_model import LinearRegression, Ridge, Lasso, LogisticRegression
     
         if mode == 'reg':
             if regular_mode == 'simple':
-                ml_model = LinearRegression(regular_param)
+                ml_model = LinearRegression()
             elif regular_mode == 'l1':
                 ml_model = Lasso(regular_param)
             elif regular_mode == 'l2':
@@ -93,8 +131,16 @@ class sklearn_model:
         
         ml_method_ext = ' ({} with {})'.format(mode, regular_mode) if mode is not None else ''
         self._reporting(sys._getframe().f_code.co_name + ml_method_ext, ml_model)
+
+        if grid_search == 'y':
+            if mode == 'reg' and regular_mode == 'simple':
+                print('No grid search is done on simple linear regression')
+            else:
+                grid_param = self._grid_param_ridge_lasso if mode == 'reg' else self._grid_param_logistic_reg
+                print('Grid search conditions: {}'.format(grid_param))
+                self._grid_search(sklearn_model=ml_model, grid_param=grid_param, mode = mode)
     
-    def sklearn_decision_tree(self, mode = 'cls', max_depth = 'None', plot_feature_importance = 'n'):
+    def sklearn_decision_tree(self, mode = 'cls', max_depth = 'None', grid_search='y'):
         from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
 
         if mode == 'reg':
@@ -110,21 +156,13 @@ class sklearn_model:
         ml_method_ext = ' ({})'.format(mode) if mode is not None else ''
         self._reporting(sys._getframe().f_code.co_name + ml_method_ext, ml_model)
 
-        # Check feature importance
-        if plot_feature_importance == 'y':
-            import matplotlib.pyplot as plt
-            import numpy as np
-            print('Feature importance {}:'.format(ml_model.feature_importances_))
-            # Plot feature importance
-            n_features = x_pd_dataframe.columns.shape[0]
-            plt.barh(range(n_features), ml_model.feature_importances_, align='center')
-            plt.yticks(np.arange(n_features), x_pd_dataframe.columns)
-            plt.xlabel('Feature importance')
-            plt.ylabel('Feature')
-            plt.show()
+        if grid_search == 'y':
+            grid_param = self._grid_param_dt
+            print('Grid search conditions: {}'.format(grid_param))
+            self._grid_search(sklearn_model=ml_model, grid_param=grid_param, mode = mode)
 
     
-    def sklearn_random_forest(self, mode = 'cls', max_depth = 'None', n_estimators=10, plot_feature_importance = 'n'):
+    def sklearn_random_forest(self, mode = 'cls', max_depth = 'None', n_estimators=10, grid_search = 'y'):
         from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
     
         if mode == 'reg':
@@ -139,20 +177,25 @@ class sklearn_model:
         ml_method_ext = ' ({})'.format(mode) if mode is not None else ''
         self._reporting(sys._getframe().f_code.co_name + ml_method_ext, ml_model)
     
-        # Check feature importance
-        if plot_feature_importance == 'y':
-            import matplotlib.pyplot as plt
-            import numpy as np
-            print('Feature importance {}:'.format(ml_model.feature_importances_))
-            # Plot feature importance
-            n_features = x_pd_dataframe.columns.shape[0]
-            plt.barh(range(n_features), ml_model.feature_importances_, align='center')
-            plt.yticks(np.arange(n_features), x_pd_dataframe.columns)
-            plt.xlabel('Feature importance')
-            plt.ylabel('Feature')
-            plt.show()
+        ## Check feature importance
+        #if plot_feature_importance == 'y':
+        #    import matplotlib.pyplot as plt
+        #    import numpy as np
+        #    print('Feature importance {}:'.format(ml_model.feature_importances_))
+        #    # Plot feature importance
+        #    n_features = x_pd_dataframe.columns.shape[0]
+        #    plt.barh(range(n_features), ml_model.feature_importances_, align='center')
+        #    plt.yticks(np.arange(n_features), x_pd_dataframe.columns)
+        #    plt.xlabel('Feature importance')
+        #    plt.ylabel('Feature')
+        #    plt.show()
+
+        if grid_search == 'y':
+            grid_param = self._grid_param_rf
+            print('Grid search conditions: {}'.format(grid_param))
+            self._grid_search(sklearn_model=ml_model, grid_param=grid_param, mode = mode)
     
-    def sklearn_svm(self, mode = 'reg', pen_param = 1.0):
+    def sklearn_svm(self, mode = 'reg', pen_param = 1.0, grid_search = 'y'):
         from sklearn.svm import SVR, SVC
     
         if mode == 'reg':
@@ -166,3 +209,8 @@ class sklearn_model:
 
         ml_method_ext = ' ({})'.format(mode) if mode is not None else ''
         self._reporting(sys._getframe().f_code.co_name + ml_method_ext, ml_model)
+
+        if grid_search == 'y':
+            grid_param = self._grid_param_svm
+            print('Grid search conditions: {}'.format(grid_param))
+            self._grid_search(sklearn_model=ml_model, grid_param=grid_param, mode = mode)
