40ed17419890085927e324508129e8fd9ade9f17
chlin907 <chiahui.lin0816@gmail.com>
2018-08-09 01:08:25 -0700

    update md

--- README.md
+++ README.md
@@ -1,7 +1,21 @@
-# Machine Learnining Template by Scikit-learn and pandas
-Concise template to run machine learning various models on Scikit-learn standard datasets.
+# Machine Learnining Baseline Dashboard
+This project presents a quick performance evaluation across few basic models and is aimed o  serve as a concise databoard before heavy feature engineering and model tuning. The machine package and data format are based on [Scikit-learn ](http://scikit-learn.org/stable/)and [Pandas](https://pandas.pydata.org/). 
 
-python3 main.py
+Five basic supervised learning models are selected: k nearest neighbor, linear model (linear regression and logistic regression), decision tree, random forest and support vector machine. Users only need to prepare feature matrix and labels and can obtain the basics evaluation metrics as the screenshot below.
 
-Output:
-Training, test and cross validation scores of various models.
+![screenshot](./misc/screenshot.png)
+
+Grid search is also avaialable across certain pre-defined parameter ranges to perfomance quick hyper-parameter optimization. 
+
+![screenshot_gs](./misc/screenshot_gs.png)
+
+# Usage
+
+* main.py: Example of parameters and how use SklearnModel.py
+* SklearnModel.py: Collection of basic models
+* SklearnDatasetSelector.py: Collection of three Scikit-learn classical datasets 
+
+User can choose either use import Scikit-learn tutorial datasets collected in SklearnDatasetSelector.py or prepare their own training data and feed to
+
+- x_pd_dataframe: Feature matrix in Pandas dataframe
+- y_pd_series: Data label in Pandas series
--- main.py
+++ main.py
@@ -1,66 +1,83 @@
-# Template to perform various classificaiton and regression with Scikit-learn and pandas 
-# Parameters are definied in "Define hyperparameters and models, and applicable datasets"
+# Template to perform various classification and regression with Scikit-learn and pandas
 #
-# Available Dataset: Breast cancer, Iris and Boston housing price
-# Method: knn, linear model, deicsion tree, random forest, svm
-import matplotlib.pyplot as plt
+# Dataset avail in SklearnDatasetSelector: Breast cancer, Iris and Boston housing price
+# Model avail in SklearnModel: knn, linear model, decision tree, random forest, svm
 import pandas as pd
-import sklearn_helper
+import SklearnModel
+import SklearnDatasetSelector
 
-def main():
-    #================================ Define hyperparameters and models 
-    # Dataset and problem statement
-    dataset_num = 3        # Int:     1 for 'breast_cancer'; 2 for 'iris'; 3 for 'boston'
-                           # Remark:  'brest_caner' is binary classfication for ['malignant' 'benign'] tumor
-                           #          'iris' is multiclass classfication for ['setosa' 'versicolor' 'virginica'] iris types
-                           #          'boston' is regression example to predict Boston housing price
-    mode = 'reg'           # Options: 'cls' for classification and 'reg' for regression
+if __name__ == '__main__':
+    # Use Sklearn dataset
+    """
+    Remark:  'brest_caner' is binary classfication for ['malignant' 'benign'] tumor
+             'iris' is multi-class classfication for ['setosa' 'versicolor' 'virginica'] iris types
+             'boston' is regression example to predict Boston housing price
+    """
+    """
+    Users can overwrite the dataset by preparing feature matrix to
+    x_pd_dataframe = Feature matrix in Pandas dataframe
+    y_pd_series = data label in Pandas series
     
-    run_grid_search = 'n'  # Str:     'y' or 'n'. To run grid search some default hyper parametes
-    scaler_std = 'n'      # Str:     'y' or 'n'. To enable scaling by mean and std
-    # knn parameters
-    num_neighbors = 2   # Definition.  Float. number of neighbors for knn
+    """
+    ######################### Beginning of input settings
+    #   dataset_name: Str. 'breast_cancer' 'iris' 'boston'
+    dataset_name = 'breast_cancer'
+
+    # Job control options
+    #   mode: 'cls' for classification and 'reg' for regression
+    #   run_grid_search: Boolean. To run grid search pre-defined default hyper parameters
+    #   run_scaler_std: Boolean. To enable scaling by mean and std
+    mode = 'reg'
+    run_grid_search = True
+    run_scaler_std = True
+
+    # Mode control options
+    #   knn parameters:
+    #   num_neighbors: Float. Number of neighbors for knn
+    num_neighbors = 2
 
     # Linear model parameters
-    reg_mode = 'simple'     # Options:    Str. 'simple, 'l1', 'l2'. 
-                        # Remark:     'l1' and 'l2' mean Lasso and Ridge regressoins.
-    reg_param = 1       # Definition: Float. Regularization parameter
-                        # Remark:     Represent alpha value in linear regression and is proportional to regularizaton strength
-                        #             Represent C value in logistic regression and INVERSELY proportional to regularization strength
-                        #             Be aware the reg_param definition difference
-    # Decision tree parameters
-    num_max_depth = None   # Definition: Int or None. Max depth of trees
+    #   reg_mode: Str. 'simple', 'l1', 'l2'
+    #   reg_param: Float. Regularization parameter
+    #              Represent alpha value in linear regression (proportional to regularization strength)
+    #              Represent C value in logistic regression (INVERSELY proportional to regularization strength)
+    reg_mode = 'l2'
+    reg_param = 1
 
-    # Random forest parameters
-    num_max_depth = None   # Definition: Int or None. Max depth of trees
-    num_estimator = 100    # Definition: Int. Num of tress to construct a forest
+    # Decision tree param
+    #   num_max_depth: Int or None. Max depth of trees
+    num_max_depth = None
+
+    # Random forest and Decision tree parameters
+    #   num_max_depth: Int or None. Max depth of trees
+    #   num_estimator: Int. Num of tress to construct a forest
+    num_max_depth = None
+    num_estimator = 100
 
     # SVM parameters
-    pen_param = 0.1       # Definition: Float. Penalty parameter
+    #   pen_param: Float. Penalty parameter
+    pen_param = 0.1
 
-    #================================ Code starts
-    #================================ Data preparation: Convert features into a dataframe and label(y) into a series
-    dataset_dict = {1:'breast_cancer', 2:'iris', 3:'boston'}
-    dataset = dataset_dict[dataset_num]
-    print('Analyze dataset {}'.format(dataset))
+    #########################End of input settings
 
-    sklearn_dataset = sklearn_helper.sklearn_dataset_selector(dataset)
-    sklearn_helper.sklearn_dataset_info_print(sklearn_dataset)
+    # Code starts
 
-    x_pd_dataframe = pd.DataFrame(sklearn_dataset.data, columns = sklearn_dataset.feature_names)
-    if scaler_std == 'y':
+    # Data preparation: Convert features into a dataframe and label(y) into a series
+    print('Analyze dataset {}'.format(dataset_name))
+
+    sklearn_dataset = SklearnDatasetSelector.SklearnDatasetSelector().select(dataset_name)
+
+    x_pd_dataframe = pd.DataFrame(sklearn_dataset.data, columns=sklearn_dataset.feature_names)
+    if run_scaler_std:
         x_pd_dataframe = (x_pd_dataframe - x_pd_dataframe.mean()) / (x_pd_dataframe.std())
 
     y_pd_series = pd.Series(sklearn_dataset.target)
 
-    #================================ Modeling with various models
-    modeling = sklearn_helper.sklearn_model(x_pd_dataframe, y_pd_series)
+    # Modeling with various models
+    modeling = SklearnModel.SklearnModel(x_pd_dataframe, y_pd_series)
 
-    modeling.sklearn_knn(n_neighbors = num_neighbors, mode = mode, grid_search = run_grid_search)
-    modeling.sklearn_linear_model(mode = mode, regular_mode = reg_mode, regular_param = reg_param, grid_search = run_grid_search)
-    modeling.sklearn_decision_tree(mode = mode, max_depth = num_max_depth, grid_search = run_grid_search)
-    modeling.sklearn_random_forest(mode = mode, max_depth = num_max_depth, n_estimators = num_estimator, grid_search = run_grid_search)
-    modeling.sklearn_svm(mode = mode, pen_param = pen_param, grid_search = run_grid_search)
-
-if __name__ == '__main__':
-    main()
+    modeling.sklearn_knn(n_neighbors=num_neighbors, mode=mode, run_grid_search=run_grid_search)
+    modeling.sklearn_linear_model(mode=mode, regular_mode=reg_mode, regular_param=reg_param, run_grid_search=run_grid_search)
+    modeling.sklearn_decision_tree(mode=mode, max_depth=num_max_depth, run_grid_search=run_grid_search)
+    modeling.sklearn_random_forest(mode=mode, max_depth=num_max_depth, n_estimators=num_estimator, run_grid_search=run_grid_search)
+    modeling.sklearn_svm(mode=mode, pen_param=pen_param, run_grid_search=run_grid_search)
